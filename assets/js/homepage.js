61eccd5d8444d89f853d4beb7812c882462580f7
escowin <edwin@escowinart.com>
2022-04-16 00:10:00 -0500

    6.1.7 | code snapshot

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,15 @@
-// built-in fetch API
 var getUserRepos = function(user) {
-    // format github api url
+    // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    // make url request
+  
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
     });
-};
-
-getUserRepos();
+  };
+  
+  getUserRepos("escowin");
+  

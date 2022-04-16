a6cf5247c901fa5fb660e489872e13ae9a50b864
escowin <edwin@escowinart.com>
2022-04-16 14:57:28 -0500

    6.4.7 | code snapshot

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,21 +3,20 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
-  // grab repo name from url query string | ?repo=.../...
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
 
   if (repoName) {
-    // display repo name on page
+    // display repo name on the page
     repoNameEl.textContent = repoName;
-    // run getRepoIssues() to retrieve repo & its issues
+
     getRepoIssues(repoName);
   } else {
-    // no repo given, redirects to homepage
+    // if no repo was given, redirect to the homepage
     document.location.replace("./index.html");
   }
 };
-
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -36,7 +35,7 @@ var getRepoIssues = function(repo) {
         }
       });
     } else {
-      // if not, redirects to index.html
+      // if not successful, redirect to homepage
       document.location.replace("./index.html");
     }
   });
@@ -55,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -70,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -98,4 +95,3 @@ var displayWarning = function(repo) {
 };
 
 getRepoName();
-// getRepoIssues();

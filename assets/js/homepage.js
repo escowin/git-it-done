17c701eb2c39a3ca5075db1838165d5954a079e5
escowin <edwin@escowinart.com>
2022-04-16 15:45:09 -0500

    6.5.6 | i.html:37-39 <... data-language='...'>...</>, hp.js:111 c.log(language) no longer null

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -105,5 +106,11 @@ var getFeaturedRepos = function(language) {
   });
 };
 
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+};
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
